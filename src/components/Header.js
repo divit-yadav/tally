a934e46e6ab8e600195158a6ea47ca40d99111e8
Beenu Yadav <beenuyadav@Beenus-MacBook-Air.local>
2025-03-08 20:29:09 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import bugreporticon from "../assets/bugreporticon.svg"
+import modetoggleicon from "../assets/modetoggleicon.svg"
 
 const Header = () => {
   return (
     <div className="headerbox">
+      <img src= {bugreporticon}/>
       <h2>
         ta<span className="red-letters">ll</span>y
       </h2>
+      <img src= {modetoggleicon}/>
     </div>
   );
 };
