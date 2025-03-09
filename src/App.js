2506558802fbaf56c898b7aa4bc4cb4ed1ac87c5
Beenu Yadav <beenuyadav@Beenus-MacBook-Air.local>
2025-03-09 21:14:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import './App.css';
-import Header from './components/Header';
-import Balance from './components/Balance';
-import IncomeExpenses from './components/IncomeExpenses';
-import TransactionList from './components/TransactionList';
-import AddTransaction from './components/AddTransaction';
+import React from 'react'
+import Home from './Home'
 
-import { GlobalProvider } from './context/GlobalState';
-
-function App() {
+export const App = () => {
   return (
-    <GlobalProvider className="App">
-      <div className="container">
-        <Header/>
-        <div className="containerb">
-        <Balance/>
-        <IncomeExpenses/>
-        </div>
-        <TransactionList/>
-        <AddTransaction/>
-      </div>
-    </GlobalProvider>
-  );
+      <Home />
+  )
 }
-
-export default App;
+export default App
